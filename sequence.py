fc03f734cc6fdcf2237db71b0a346773e797a445
PrabinGautam <prabeen122@gmail.com>
2023-05-06 21:38:56 +0545

    Seqeunce Files Added

---
+++ sequence.py
@@ -0,0 +1,9 @@
+array = [5, 1, 22, 25, 5, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
+
+def isValidSequence(array: list[int], sequence: list[int]) -> bool:
+    pass
+
+
+print(isValidSequence(array, sequence))
