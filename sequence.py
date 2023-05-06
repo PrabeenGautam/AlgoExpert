fcd5acd29b75165a6c44bf5ad1be28e499207f36
PrabinGautam <prabeen122@gmail.com>
2023-05-06 22:06:19 +0545

    Valid Seqeunces Added

--- sequence.py
+++ sequence.py
@@ -1,9 +1,16 @@
 array = [5, 1, 22, 25, 5, 6, -1, 8, 10]
-sequence = [1, 6, -1, 10]
+sequence = [5, 1, 22, 22, 25, 5, 6, -1, 8, 10]
 
 
 def isValidSequence(array: list[int], sequence: list[int]) -> bool:
-    pass
+    seq_pointer = 0
+    for value in array:
+        if value == sequence[seq_pointer] and seq_pointer < len(sequence):
+            seq_pointer += 1
+
+        if seq_pointer == len(sequence):
+            return True
+    return False
 
 
 print(isValidSequence(array, sequence))
