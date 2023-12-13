461573f1dbf71d3a8aa0f946588c624588a5b240
PrabinGautam <prabeen122@gmail.com>
2023-12-13 15:23:01 +0545

    fix: added init setup for dsa

--- src/array/easy/two-number-sum.ts
+++ src/array/easy/two-number-sum.ts
@@ -1,3 +1,6 @@
-export default function twoNumberSum() {
-  console.log("dev");
-}
+const input = [3, 5, -4, 8, 11, 1, -1, 6];
+const target = 10;
+
+function twoNumberSum(input: Array<number>, target: number) {}
+
+export default () => twoNumberSum(input, target);
