ad82caeb40902c5299b8e7ec873d59a5941f0f6a
PrabinGautam <prabeen122@gmail.com>
2023-12-14 21:01:46 +0545

    feat: added sorted square problems

---
+++ src/array/easy/sortedsquare.ts
@@ -0,0 +1,30 @@
+const input = [1, 2, 3, 5, 6, 8, 9];
+// const input = [-6, -2, 0, 4, 5, 6];
+
+function sortedSquares(nums: number[]) {
+  // Minimal Solutions
+  /*   const sortedArray = nums.sort((a, b) => Math.abs(a) - Math.abs(b));
+  return sortedArray.map((e) => e ** 2); */
+  // Another Solutions
+
+  const total = nums.length;
+  const sortedArray = Array.from({ length: total }, () => 0);
+
+  let start = 0,
+    end = total - 1;
+
+  while (start <= end) {
+    const endSquared = nums[end] ** 2;
+    const startSquared = nums[start] ** 2;
+
+    const step = end - start;
+    const isStartLower = startSquared <= endSquared;
+
+    sortedArray[step] = isStartLower ? endSquared : startSquared;
+    isStartLower ? end-- : start++;
+  }
+
+  return sortedArray;
+}
+
+console.log(sortedSquares(input));
