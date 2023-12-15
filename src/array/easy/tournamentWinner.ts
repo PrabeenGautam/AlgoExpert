2ba335eb2a976a024c58dcdc3f59019009add88e
PrabinGautam <prabeen122@gmail.com>
2023-12-15 13:30:33 +0545

    feat: added tournament winner function

--- src/array/easy/tournamentWinner.ts
+++ src/array/easy/tournamentWinner.ts
@@ -8,6 +8,21 @@ const results = [0, 0, 1];
 
 // answers = "Python"
 
-function tournamentWinner(competitions: Array<string[]>, results: number[]) {}
+function tournamentWinner(competitions: Array<string[]>, results: number[]) {
+  const scorer = new Map();
+  let bestTeam = "";
+
+  for (let i = 0; i < results.length; i++) {
+    const winnerTeam = competitions[i][results[i] === 1 ? 0 : 1];
+    const hasWinner = scorer.get(winnerTeam);
+    scorer.set(winnerTeam, (hasWinner || 0) + 1);
+
+    if (scorer.get(winnerTeam) > (scorer[bestTeam] || 0)) {
+      bestTeam = winnerTeam;
+    }
+  }
+
+  return bestTeam;
+}
 
 console.log(tournamentWinner(competitions, results));
